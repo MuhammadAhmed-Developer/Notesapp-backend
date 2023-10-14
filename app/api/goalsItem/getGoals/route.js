65aa24e77102d495e6ceb18f56ec867cd5060a91
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-10-14 18:49:46 +0500

    backend updated

--- app/api/goalsItem/getGoals/route.js
+++ app/api/goalsItem/getGoals/route.js
@@ -4,11 +4,15 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   try {
     const items = await db.itemsListGoals.findMany();
-
+console.log(items);
+console.log(items);
+console.log(items);
+console.log(items);
     const itemsWithStrings = items.map(item => ({
       ...item,
       iid: item.iid.toString(),
     }));
+    // dlfjjjjjj
 
     return NextResponse.json({ items: itemsWithStrings }, { status: 200 });
   } catch (error) {
