7814b2cea562328f815d85e00828ea4482fcc7ac
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-10-17 18:02:46 +0500

    update api work

--- app/api/subtasks/route.js
+++ app/api/subtasks/route.js
@@ -1,48 +1,43 @@
-  // Import your Prisma client
-  import { db } from "@/lib/db";
-  import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-  // Create a new subtask for a main task
-  export async function POST(req) {
-    try {
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { label, checked, id } = body;
+    const idofmain = id; // Since id is the ID of the MainTask
 
-      const body = await req.json()
-      const {  label, checked, id } = body
-         console.log(label, checked, id);
-      const val = Object.keys(id);
-      const idofmain =  val.pop()
-
-      console.log('bengoo is also in server',Object.keys(id));
-      console.log('bengoo is also in server',(idofmain));
-
-      const newSubtask = await db.subtask.create({
-        data: {
-          label,
-          checked,
-          mainTask: {
-            connect: {
-              id: idofmain,
-            },
+    const newSubtask = await db.subGoal.create({
+      data: {
+        label,
+        checked,
+        mainGoal: {
+          connect: {
+            id: idofmain,
           },
         },
-      });
+      },
+    });
 
-      // Send the created subtask in the JSON response
-      return NextResponse.json({ message: 'Subtask added', subtask: newSubtask }, { status: 201 });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ message: 'Subtask cannot be added' }, { status: 409 });
-    }
+    return NextResponse.json(
+      { message: 'Subtask added', subtask: newSubtask },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Subtask cannot be added' }, { status: 409 });
   }
+}
+
 
   // Get all subtasks for a main task
-  export async function GET(req) {
-    try {
-      const subtasks = await db.subtask.findMany();
-      console.log('AAAAAAA',subtasks);
-      return NextResponse.json(subtasks, { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ message: 'Error fetching subtasks' }, { status: 500 });
-    }
-  }
+  // export async function GET(req) {
+  //   try {
+  //     const subtasks = await db.subtask.findMany();
+  //     console.log('AAAAAAA',subtasks);
+  //     return NextResponse.json(subtasks, { status: 200 });
+  //   } catch (error) {
+  //     console.error(error);
+  //     return NextResponse.json({ message: 'Error fetching subtasks' }, { status: 500 });
+  //   }
+  // }
