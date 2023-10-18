120a83e7673129de30d18426d612b2385b30dba6
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-10-18 14:54:36 +0500

    add line in getitem

--- app/api/items/getItems/route.js
+++ app/api/items/getItems/route.js
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"
+export const revalidate = 20
 export async function GET(req) {
   try {
     // Retrieve all items from the database
